a1db1eafbeebcc5ad4f26d33125f2ce5af19d21a
Axonick <Axonic@mail.ru>
2022-09-17 21:36:22 +0300

    Часть вторая книги

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const getNumber = (max = 11, min = 0) => Math.floor(Math.random() * (max - min) + min);
+const optimizeNumber = (n) => {
+  let str = n + '';
+  return (n + '').length === 2 ? str : '0' + n;
+}
+
 
 const data = [];
 let n = getNumber(41, 1);
@@ -8,3 +13,9 @@ while (n--) {
   data.push(getNumber(100))
 }
 console.log(data)
+
+const box = document.querySelector('.box');
+
+for (let val of data) {
+  box.innerHTML += `<div class="card">${optimizeNumber(val)}</div>`;
+}
